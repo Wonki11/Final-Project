4f758568c44b7ea12d51d03543eb380007b82445
Wonki11 <wongi5011@daum.net>
2024-07-30 18:40:39 +0900

    test

--- moviechart/src/Moviechart/Moviechart.js
+++ moviechart/src/Moviechart/Moviechart.js
@@ -1,11 +1,12 @@
 import React, {useEffect , useState} from "react";
 import axios from 'axios';
+import './Moviechart.css'
 
 const MovieChart = () => {
     const [MovieChart , setMovieChart] = useState([]);
 
     useEffect(() => {
-        axios.get('https://raw.githubusercontent.com/sangjunchung/jsonFolder/master/movie.json')
+        axios.get('https://raw.githubusercontent.com/Wonki11/moviejson/master/movies.json')
         .then(res => {
             setMovieChart(res.data.results);
         })
@@ -15,11 +16,12 @@ const MovieChart = () => {
     },[]);
 
     return(
-        <div>
+        <div className="moviechart-container">
             <h1>영화차트</h1>
-            <div>
+            <div className="moviechart-content">
                 {MovieChart.map(Movies => (
-                    <div key={Movies.id}>
+                    <div key={Movies.id} className="moviechart-movies">
+                        <img src={Movies.poster_path} />
                         <h2>{Movies.title}</h2>
                         <p>평점 : {Movies.vote_average}</p>
                         <p>개봉일 : {Movies.release_date}</p>
