ac6d9c4ba7d624e946c2ed9c94efddba8c185ea1
Wonki11 <wongi5011@daum.net>
2024-08-05 18:40:31 +0900

    update

--- moviechart/src/movieboard-app/Booking.js
+++ moviechart/src/movieboard-app/Booking.js
@@ -1,20 +1,40 @@
 import React, { useState,useEffect } from 'react';
 import { useLocation} from 'react-router-dom';
 import './Booking.css';
-import movieData from './movies.json';
+//import movieData from './movies.json';
+import axios from 'axios'; // 비동기로 axios를 사용해서 영화 데이터 로딩
 
 const Booking = () => {
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
+    const location = useLocation(); // 전 무비차트페이지에서 선택한 값을 저장후 예매티켓으로 넘어오게끔 지정
+    const queryParams = new URLSearchParams(location.search); // 쿼리파람으로 전 페이지의 내용 서치
     const movieId = queryParams.get('movieId');
-    const [movies, setMovies] = useState(movieData.results);
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
     const [selectedSeat, setSelectedSeat] = useState([]);
     const [numPeople, setNumPeople] = useState(1);
+    const [selectedRegion, setSelectedRegion] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedTime, setSelectedTime] = useState(null);
 
     useEffect(() => {
-        if (movieId) {
-            const movie = movies.find(m => m.id === parseInt(movieId));
+       const fetchMovies = async () => {
+        try{
+            const response = await axios.get('https://raw.githubusercontent.com/Wonki11/moviejson/master/movies.json');
+            setMovies(response.data.results);
+            if(movieId) {
+                const movie = response.data.results.find(m => m.id === parseInt(movieId));
+                setSelectedMovie(movie)
+            }
+        } catch (err) {
+            console.error("Error loading movie data: ", err);
+        }
+       };
+       fetchMovies();
+    },[movieId]);
+
+    useEffect(() => { // useLocation과 연계 무비차트페이지에서 선택한 movieId 값을 json 파일에서 확인후 데이터값을 가지고 넘어옴 (해당 영화 예매하기 버튼 누르면 예매티켓에서 그 영화 예매하는 걸로 바로 시작)
+        if (movieId && movies.length > 0) {
+            const movie = movies.find(m => m.id === parseInt(movieId)); // parseInt : 함수는 문자열 인자를 파싱하여 특정 진수(수의 진법 체계에서 기준이 되는 값)의 정수를 반환
             setSelectedMovie(movie);
         }
     }, [movieId,movies]);
@@ -33,16 +53,44 @@ const Booking = () => {
 
     const handleNumPeopleChange = (event) => {
         const value = parseInt(event.target.value);
-        if (value >= 1) {
+        if (!isNaN(value) && value >= 1 && value <= 4) {
             setNumPeople(value);
             setSelectedSeat([]);
+        } 
+    };
+
+     const getPosterPath = (movie) => {
+        const path = movie.poster_path;
+        console.log('Poster path:', path); // 경로 확인용 로그
+        return path;
+    };
+
+    const handleRegionChange = (region) => {
+        setSelectedRegion(region);
+    };
+
+    const handleDateChange = (e) => {
+        setSelectedDate(e.target.value);
+        setSelectedTime(null) // 날짜 변경 시 선택한 시간 초기화
+    };
+
+    const handleTimeChange = (time) => {
+        setSelectedTime(time);
+    }
+
+    const WeekDate = () => {
+        const options = [];
+        const today = new Date(); 
+        for (let i = 0; i <= 7; i++){
+            const date = new Date(today);
+            date.setDate(today.getDate()+ i);
+            const dateString = date.toISOString().split('T')[0];
+            options.push(dateString);
         }
+        return options;
     };
 
-    const getPosterPath = (movie) => {
-        return `${process.env.PUBLIC_URL}/movieimages/${movie.poster_path}`;
-    };
-
+  
     return (
         <div className="booking">
             <div className="movie-list">
@@ -60,9 +108,10 @@ const Booking = () => {
                             <div className="movie-info">
                                 <img src={getPosterPath(selectedMovie)} alt="Movie Poster" />
                                 <div className="movie-details">
-                                    <p>영화: {selectedMovie.title}</p>
-                                    <p>관람일시: 2019-11-20 10:40</p>
-                                    <p>선택좌석: {selectedSeat.length > 0 ? selectedSeat.join(', ') : '없음'}</p>
+                                    <p>영화 : {selectedMovie.title}</p>
+                                    <p>영화관 : {selectedRegion}</p>
+                                    <p>관람일시 : {selectedDate} {selectedTime}</p>
+                                    <p>선택좌석 : {selectedSeat.length > 0 ? selectedSeat.join(', ') : '없음'}</p>
                                 </div>
                             </div>
                             <div className="total-price">
@@ -79,18 +128,25 @@ const Booking = () => {
                 <div className="steps-row">
                     <div className="step">
                         <p>STEP1: 영화관 선택</p>
-                        <button className="step-button">서울</button>
-                        <button className="step-button">경기</button>
+                        <button className="step-button" onClick={() => handleRegionChange('강남')}>강남</button>
+                        <button className="step-button" onClick={() => handleRegionChange('역삼')}>역삼</button>
                     </div>
                     <div className="step">
                         <p>STEP2: 관람일 선택</p>
-                        <button className="step-button">2019-11-20</button>
+                        <select value={selectedDate} onChange={handleDateChange}>
+                            <option value="">날짜를 선택하세요</option>
+                            {WeekDate().map(date => (
+                                <option key={date} value={date}>{date}</option>
+                            ))}
+                        </select>
                     </div>
                     <div className="step">
                         <p>STEP3: 관람시간 선택</p>
-                        <button className="step-button">10:40</button>
-                        <button className="step-button">13:45</button>
-                        <button className="step-button">17:00</button>
+                        <button className="step-button" onClick={() => handleTimeChange('10:40')}>10:40</button>
+                        <button className="step-button" onClick={() => handleTimeChange('13:45')}>13:45</button>
+                        <button className="step-button" onClick={() => handleTimeChange('17:00')}>17:00</button>
+                        <button className="step-button" onClick={() => handleTimeChange('19:40')}>19:40</button>
+                        <button className="step-button" onClick={() => handleTimeChange('22:20')}>22:20</button>
                     </div>
                     <div className="step">
                         <p>STEP4: 좌석 및 잔여석 확인</p>
@@ -98,6 +154,7 @@ const Booking = () => {
                             <input 
                                 type="number" 
                                 min="1" 
+                                max="4"
                                 value={numPeople} 
                                 onChange={handleNumPeopleChange} 
                             />
@@ -131,6 +188,7 @@ const Booking = () => {
                         <li>영화 예매는 관람일 전날 취소 시 수수료 없이 취소 가능합니다.</li>
                         <li>상영관 입장은 상영시간 10분 전부터 가능합니다.</li>
                         <li>할인혜택은 중복적용이 불가합니다.</li>
+                        <li>좌석은 한 계정당 4자리만 예매 가능합니다.</li>
                     </ol>
                 </div>
                 <button className="confirm-button">결제</button>
